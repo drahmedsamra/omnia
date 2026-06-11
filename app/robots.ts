dde7c5f2277d3f16aeb1d7598d3fad5f1743803c
Ahmed Samra <drahmedsamra@gmail.com>
2026-06-11 20:56:19 +0300

    Add professional timeline section

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://omnia.ahmedsamra.com/sitemap.xml",
+  };
+}
