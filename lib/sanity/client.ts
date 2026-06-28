0b71559d859430c4e413ea0979f668e95df6613b
Ahmed Samra <drahmedsamra@gmail.com>
2026-06-28 19:46:39 +0300

    Remove Sanity CMS and News system

--- lib/sanity/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "next-sanity";
-
-export const client = createClient({
-  projectId: "flajunq1",
-  dataset: "production",
-  apiVersion: "2026-06-27",
-  useCdn: false,
-});
--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     qualities: [60, 75, 80],
-
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-      },
-    ],
   },
 };
 
