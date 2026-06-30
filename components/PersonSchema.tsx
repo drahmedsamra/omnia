fa5fe47d69b45c2a872e0b0489c5c33a7930e8ad
Ahmed Samra <drahmedsamra@gmail.com>
2026-06-30 16:08:22 +0300

    Add Person Schema.org structured data

--- components/PersonSchema.tsx
+++ components/PersonSchema.tsx
@@ -2,18 +2,26 @@ export default function PersonSchema() {
   const schema = {
     "@context": "https://schema.org",
     "@type": "Person",
-    name: "أمنية سمرة",
-    alternateName: "Omnia Samra",
+
+    name: "أمنية أحمد سمرة",
+    alternateName: "Omnia Ahmed Samra",
+
     url: "https://omnia.ahmedsamra.com",
+
     image: "https://omnia.ahmedsamra.com/og-image.webp",
+
+    description:
+      "لاعبة منتخب مصر للتنس ولاعبة النادي الأهلي.",
+
+    gender: "Female",
+
     nationality: {
       "@type": "Country",
       name: "Egypt",
     },
-    gender: "Female",
+
     jobTitle: "Tennis Player",
-    description:
-      "لاعبة منتخب مصر للتنس ولاعبة النادي الأهلي.",
+
     memberOf: [
       {
         "@type": "SportsOrganization",
@@ -24,7 +32,6 @@ export default function PersonSchema() {
         name: "النادي الأهلي",
       },
     ],
-    sport: "Tennis",
   };
 
   return (
